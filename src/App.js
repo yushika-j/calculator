0b60f64e24854ec0c36f071ca8a848e43fa8d709
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-30 18:51:44 -0400

    created files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-grid">
+      <h1>Calculator</h1>
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <div className="numbers">
+        <button className="AC">AC</button>
+        <button className="division">/</button>
+      </div>
     </div>
   );
 }
