e249ee0bdbc5c010e96cde32c752946638ce6ff2
Yushika Jhundoo <pjhun035@uottawa.ca>
2024-03-30 20:01:29 -0400

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 
 import './App.css';
+import { useReducer } from 'react'
 
+const ACTIONS ={
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}){
+  switch(type) {
+    case: ACTIONS.ADD_DIGIT
+  }
+}
 function App() {
+  const[{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,{})
+
   return (
     <div className="calculator-grid">
-      <h1>Calculator</h1>
+      
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
-      <div className="numbers">
-        <button className="AC">AC</button>
-        <button className="division">/</button>
+        <button className="span-two">AC</button>
+        <button className="delete">DEL</button>
+        <button>/</button>
+        
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>x</button>
+        
+
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>-</button>
+
+        <button>.</button>
+        <button>0</button>
+        <button className="span-two">=</button>
+        
       </div>
-    </div>
+    
   );
 }
 
